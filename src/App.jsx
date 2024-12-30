d261b4a0c273b85f3864eba7f3dd154ef3d6c4e5
Mamunur Roshid <69715351+mamunurroshid90@users.noreply.github.com>
2024-12-30 17:05:13 +0600

    added recommended section

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <Nav />
-      <Products />
       <Recomended />
+      <Products />
     </>
   );
 }
--- src/recomended/Recomended.jsx
+++ src/recomended/Recomended.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Recomended = () => {
-  return <div>Recommended</div>;
+  return (
+    <>
+      <div className=" container mx-auto max-w-[1280px] py-6 ">
+        <h2 className=" text-2xl font-bold font-sans">Recommended</h2>
+        <div className=" flex gap-4 mt-4">
+          <button className=" border rounded px-6 py-2 font-semibold">
+            All Products
+          </button>
+          <button className=" border rounded px-6 py-2 font-semibold">
+            Nike
+          </button>
+          <button className=" border rounded px-6 py-2 font-semibold">
+            Adidas
+          </button>
+          <button className=" border rounded px-6 py-2 font-semibold">
+            Puma
+          </button>
+          <button className=" border rounded px-6 py-2 font-semibold">
+            Vans
+          </button>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Recomended;
